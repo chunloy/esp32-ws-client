f59222f8b0bfe1bbce92e6c754e3df00ed8645bc
Aaron Y <101907461+chunloy@users.noreply.github.com>
2023-01-03 19:42:17 -0800

    setup wifi

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include <WiFiMulti.h>
+#include <secrets.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define LED_PIN 14
+
+WiFiMulti wifiMulti;
+
+void initWiFi()
+{
+  wifiMulti.addAP(SSID, PASS);
+
+  while (wifiMulti.run() != WL_CONNECTED)
+  {
+    delay(100);
+  }
+
+  Serial.println("Connected");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  initWiFi();
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
 }
